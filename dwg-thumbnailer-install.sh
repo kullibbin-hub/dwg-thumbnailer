49009e379459070f40cf554a238bb287f519e764
kullibbin-hub <kullibbin@gmail.com>
2026-03-05 10:27:39 +0300

    Add files via upload

--- dwg-thumbnailer-install.sh
+++ dwg-thumbnailer-install.sh
@@ -2,19 +2,23 @@
 set -e
 
 # 1. Скачивание архива
+echo 'Скачиваем NConvert
+'
 cd /tmp
 wget https://download.xnview.com/NConvert-linux64.tgz -O NConvert-linux64.tgz
 
 # 2. Распаковка в /usr/local
-sudo rm -rf /usr/local/NConvert
+#sudo rm -rf /usr/local/NConvert
 sudo tar -xzf NConvert-linux64.tgz -C /usr/local
-sudo mv /usr/local/NConvert-linux64 /usr/local/NConvert
+#sudo mv /usr/local/NConvert-linux64 /usr/local/NConvert
 
 # 3. Симлинк в /usr/local/bin
 sudo rm -f /usr/local/bin/nconvert
 sudo ln -s /usr/local/NConvert/nconvert /usr/local/bin/nconvert
 
 # 4. Скрипт dwg-thumbnail.sh
+echo 'Создаем /usr/local/bin/dwg-thumbnail.sh
+'
 sudo tee /usr/local/bin/dwg-thumbnail.sh >/dev/null <<'EOF'
 #!/bin/bash
 INPUT="$1"
@@ -39,6 +43,8 @@ EOF
 sudo chmod +x /usr/local/bin/dwg-thumbnail.sh
 
 # 5. Файл dwg.thumbnailer
+echo 'Создаем /usr/share/thumbnailers/dwg.thumbnailer
+'
 sudo tee /usr/share/thumbnailers/dwg.thumbnailer >/dev/null <<'EOF'
 [Thumbnailer Entry]
 TryExec=/usr/local/bin/dwg-thumbnail.sh
@@ -48,7 +54,10 @@ Flags=NoCopy
 EOF
 
 # 6. Очистка кэша миниатюр
+echo 'Очистка кэша миниатюр
+'
 rm -rf ~/.cache/thumbnails/*
 
-echo "Установка завершена. Перезапусти Nautilus: nautilus -q"
+echo "Установка завершена. Теперь желательно перезайти в сессию
+или перезагрузить компьютер."
 
